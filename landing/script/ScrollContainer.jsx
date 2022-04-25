e1fb36bd4c6be0ed9fd76640d03811283a54e713
Julien Zh <34554597+MrSnoobydoo@users.noreply.github.com>
2022-04-25 20:30:00 +0200

    Initial commit 2

---
+++ landing/script/ScrollContainer.jsx
@@ -0,0 +1,15 @@
+class ScrollContainer
+{
+    constructor({ index, title, description, img, currentPage, totalPage })
+    {
+
+    }
+
+    ScrollMenu(){
+        return (
+            <div class="scroll_menu">
+
+            </div>
+        )
+    }
+}
