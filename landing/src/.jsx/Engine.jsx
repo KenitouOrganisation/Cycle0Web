aee0570cf17db357be9318eb82ff4498cf534c70
Gigorta <tech.b56@gmail.com>
2022-04-28 21:37:29 +0200

    Static Version

--- landing/src/.jsx/Engine.jsx
+++ landing/src/.jsx/Engine.jsx
@@ -1,83 +1,115 @@
-const Engine = {}
+const Engine = {};
+Engine.JSEnable = false;
 
-Engine.CheckCompatibility = function(){
+Engine.CheckCompatibility = function () {
     const value = !Object.defineProperty || !document.addEventListener;
 
-    if(value === true)
-        console.error(new Error('The browser is not supported (too old or missing Javascript)'));
+    if (value === true)
+        console.error(
+            new Error(
+                "The browser is not supported (too old or missing Javascript)"
+            )
+        );
 
-    return value;
+    return !value;
 };
 
-Engine.Alert = class _Alert
-{
-    static Show({ title, message, style }){
-        alert(title  + "\n\n" + message);
+Engine.Console = class _Console {
+    static __dev__ = true;
+    static Log(...args){
+        if(_Console.__dev__ === true)
+            console.log(...args);
+    }
+};
+
+Engine.Alert = class _Alert {
+    static Show({ title, message, style }) {
+        alert(title + "\n\n" + message);
     }
 
-    static Error(obj){
+    static Error(obj) {
         _Alert.Show(obj);
     }
 };
 
-(function(){
+(function () {
     // protected env for defining static/const attribute in Engine (make them not rewritable for safety)
-    'use strict';
+    "use strict";
 
-
-    if(Engine.CheckCompatibility() === true)
+    if (Engine.CheckCompatibility() === false)
         return Engine.Alert.Error({
-            title : 'Navigateur incompatible',
-            message : "Votre navigateur n'est pas supporté par notre site. Veuillez le mettre à jour ou changer de navigateur."
+            title: "Navigateur incompatible",
+            message:
+                "Votre navigateur n'est pas supporté par notre site. Veuillez le mettre à jour ou changer de navigateur.",
         });
 
-    function Define(obj, attrName, attrValue){
+    function Define(obj, attrName, attrValue) {
         // const properties in obj : https://stackoverflow.com/questions/19744725/is-it-possible-to-create-read-only-members-in-javascript-object-literal-notation
-        Object.defineProperty(obj, attrName, {  // FIXME : weakspot, this function can be redefined and break the Engine code
+        Object.defineProperty(obj, attrName, {
+            // FIXME : weakspot, this function can be redefined and break the Engine code
             configurable: false,
             writable: false,
-            enumerable : false,
-            value: attrValue
+            enumerable: false,
+            value: attrValue,
         });
     }
 
     // defining the Define function inside Engine as static function
-    Define(Engine, 'Define', Define);
-    Define(Engine, 'Def', function(attrName, attrValue){
-        return Define(Engine, attrName, attrValue)
+    Define(Engine, "Define", Define);
+    Define(Engine, "Def", function (attrName, attrValue) {
+        return Define(Engine, attrName, attrValue);
     });
 })();
 
+Engine.CSS = {};
+Engine.DOM = {};
 
-function _AddJsAttr(elmt, jsAttrObj){
-    for(let name in jsAttrObj){
-        elmt[name] = jsAttrObj[name]
+Engine.AddJsAttr = function (elmt, jsAttrObj) {
+    for (let name in jsAttrObj) {
+        elmt[name] = jsAttrObj[name];
     }
-}
+};
 
 /**
- * 
- * @param {string} name 
- * @param {Object} attr 
+ *
+ * @param {string} name
+ * @param {Object} attr
  *      attr => { js-attr } for javascript attribute (doesn't appear on the html dom)
- * @param  {DOMElement} children 
+ * @param  {DOMElement} children
  * @returns element created
  */
-Engine.Elmt = function (name, attr, ...children){
-
+Engine.Elmt = function (name, attr, ...children) {
     const elmt = document.createElement(name);
-    for(let name in attr){
-        if(name == 'js-attr')
-            _AddJsAttr(elmt, attr[name])
-        else
-            elmt.setAttribute(name, attr[name]);
+    for (let name in attr) {
+        if (name == "data-js-attr") Engine.AddJsAttr(elmt, attr[name]);
+        else elmt.setAttribute(name, attr[name]);
     }
 
-    children.forEach(child => {
-        if(child instanceof Node)
-            elmt.appendChild(child)
-        
+    children.forEach((child) => {
+        if (child instanceof Node) elmt.appendChild(child);
     });
     return elmt;
+};
 
+Engine.Q = (selector) => document.querySelector(selector);
+Engine.QAll = (selector) => document.querySelectorAll(selector);
+
+Engine.CSS._root = null;
+Engine.CSS.SetVar = function (name, value) {
+    if (!Engine.CSS._root) Engine.CSS._root = Engine.Q(":root");
+
+    Engine.CSS._root.style.setProperty(name, value);
+};
+
+Engine.DOM._readyElement = "#ready";
+Engine.DOM.OnReady = Engine.OnReady = (callback) => {
+    document.addEventListener("DOMContentLoaded", (e) => {
+        const interval = setInterval(() => {
+            const readyElmt = Engine.Q(Engine.DOM._readyElement);
+            if (readyElmt) {
+                clearInterval(interval);
+                callback();
+            }
+        }, 100);
+    });
 };
--- landing/src/.jsx/zMain.jsx
+++ landing/src/.jsx/zMain.jsx
@@ -1,1 +1,19 @@
-const t = new ScrollContainer()
+// Const
+const CSSHeightResize = () =>
+    Engine.CSS.SetVar("--page-height", window.innerHeight + "px");
+
+// Init Call
+if(Engine.JSEnable)
+    CSSHeightResize();
+
+Engine.OnReady(() => {
+
+    if(Engine.CheckCompatibility() === false || !Engine.JSEnable)
+        return;
+
+    const _E = Engine;
+    const _Log = Engine.Console.Log;
+
+    window.addEventListener('resize', CSSHeightResize);
+
+});
